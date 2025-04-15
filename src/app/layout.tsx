cf09ea0ef403f49c917b9d07ef47f54f90ea01c4
Cristian-F-M <cfmorales.diaz@gmail.com>
2025-04-15 17:32:19 -0500

    [style]: Format files

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,24 +1,23 @@
-import type { Metadata } from "next";
-import "./globals.css";
+import type { Metadata } from 'next'
+import './globals.css'
 
 export const metadata: Metadata = {
-  title: "Votaciones - Vota en línea por tu candidato preferido en CGAO",
-  description: "Vota en línea por tu candidato de la jornada preferido en el Centro de gestión agroempresarial del oriente (CGAO, SENA)", 
-};
+	title: 'Votaciones - Vota en línea por tu candidato preferido en CGAO',
+	description:
+		'Vota en línea por tu candidato de la jornada preferido en el Centro de gestión agroempresarial del oriente (CGAO, SENA)'
+}
 
 export default function RootLayout({
-  children,
+	children
 }: Readonly<{
-  children: React.ReactNode;
+	children: React.ReactNode
 }>) {
-  return (
-    <html lang="es">
-      <head>
-        <link rel="icon" type="image/svg+xml" href="./logo-sena.svg" />
-      </head>
-      <body>
-        {children}
-      </body>
-    </html>
-  );
+	return (
+		<html lang="es">
+			<head>
+				<link rel="icon" type="image/svg+xml" href="./logo-sena.svg" />
+			</head>
+			<body>{children}</body>
+		</html>
+	)
 }
