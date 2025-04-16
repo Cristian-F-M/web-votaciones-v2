b632b7d747e4da6db8188d3ca8bb6c7951d798f3
Cristian-F-M <cfmorales.diaz@gmail.com>
2025-04-16 16:22:25 -0500

    [refactor]: Refactor logic in input component to reuse div wrapper

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,103 +1,94 @@
 import { twMerge } from 'tailwind-merge'
 import type { InputProps } from '@/types/input'
 
-export function Input(props: InputProps) {
-	if (props.typeInput === 'select') {
-		const {
-			id,
-			name,
-			label,
-			required,
-			error,
-			items,
-			typeInput,
-			selectedItem = '0',
-			className,
-			...rest
-		} = props
-
-		return (
+export function Input({
+	id,
+	name,
+	label,
+	required,
+	error,
+	typeInput,
+	className,
+	...restProps
+}: InputProps) {
+	return (
+		<div>
 			<div
 				className="input w-full relative flex flex-col"
 				aria-labelledby={`${id}-label`}
 			>
-				<select
-					{...rest}
-					id={id}
-					name={name}
-					required={required}
-					defaultValue={selectedItem}
-					className={twMerge([
-						'w-full text-gray-800 text-sm border-b border-gray-400 border-solid outline-none',
-						className
-					])}
-				>
-					<option value="0" disabled>
-						{label}
-					</option>
-					{items.map((item) => (
-						<option key={item.id} value={item.value}>
-							{item.name}
-						</option>
-					))}
-				</select>
+				{(() => {
+					const { selectedItem, items, ...restPropsSelect } =
+						restProps as Extract<InputProps, { typeInput: 'select' }>
+					if (typeInput === 'select') {
+						return (
+							<select
+								{...restPropsSelect}
+								id={id}
+								name={name}
+								required={required}
+								defaultValue={selectedItem}
+								className={twMerge([
+									'w-full text-gray-800 text-sm border-b border-gray-400 border-solid outline-none',
+									className
+								])}
+							>
+								<option value="0" disabled>
+									{label}
+								</option>
+								{items.map((item) => (
+									<option key={item.id} value={item.value}>
+										{item.name}
+									</option>
+								))}
+							</select>
+						)
+					}
+				})()}
+
+				{(() => {
+					if (typeInput === 'text') {
+						const { type, ...restPropsInput } = restProps as Extract<
+							InputProps,
+							{ typeInput: 'text' }
+						>
+						return (
+							<>
+								<input
+									{...restPropsInput}
+									id={id}
+									name={name}
+									type={type}
+									required={required}
+									placeholder=" "
+									className={twMerge([
+										'w-full text-gray-600 text-sm border-b border-gray-400 border-solid outline-none',
+										className
+									])}
+								/>
+								<label
+									id={`${id}-label`}
+									htmlFor={id}
+									className="text-gray-500 text-sm pointer-events-none absolute bottom-px flex flex-row items-center gap-px"
+								>
+									{label}
+									{required && (
+										<span className="text-red-600 text-lg leading-3">*</span>
+									)}
+								</label>
+							</>
+						)
+					}
+				})()}
 			</div>
-		)
-	}
-
-	// Input tipo "text"
-	const {
-		id,
-		name,
-		label,
-		error,
-		required,
-		className,
-		typeInput,
-		type = 'text',
-		...rest
-	} = props
-
-	return (
-		<>
-			<div
-				className="relative flex flex-col input w-full"
-				aria-labelledby={`${id}-label`}
+			<p
+				id="document-error"
+				role="alert"
+				aria-live="assertive"
+				className={`error-message text-xs text-red-600 mt-px ${error ? 'opacity-100' : 'opacity-0'}`}
 			>
-				<input
-					{...rest}
-					id={id}
-					name={name}
-					type={type}
-					required={required}
-					placeholder=" "
-					className={twMerge([
-						'w-full text-gray-600 text-sm border-b border-gray-400 border-solid outline-none',
-						className
-					])}
-				/>
-				<label
-					id={`${id}-label`}
-					htmlFor={id}
-					className="text-gray-500 text-sm pointer-events-none absolute bottom-px flex flex-row items-center gap-px"
-				>
-					{label}
-					{required && (
-						<span className="text-red-600 text-lg leading-3">*</span>
-					)}
-				</label>
-			</div>
-
-			{error && (
-				<p
-					id="document-error"
-					role="alert"
-					aria-live="assertive"
-					className="error-message text-xs text-red-600 mt-px"
-				>
-					{error}
-				</p>
-			)}
-		</>
+				{error}
+			</p>
+		</div>
 	)
 }
