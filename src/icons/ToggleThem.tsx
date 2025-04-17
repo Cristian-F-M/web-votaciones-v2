de479ae660ee2eec6bcd32c50884813abc93deff
Cristian-F-M <cfmorales.diaz@gmail.com>
2025-04-17 11:48:23 -0500

    [chore]: Add toggleTheme component

---
+++ src/icons/ToggleThem.tsx
@@ -0,0 +1,32 @@
+'use client'
+import { useCallback, useEffect, useRef } from 'react'
+import Moon from './Moon'
+import Sun from './Sun'
+
+export function ToggleTheme() {
+	const toggleThemeRef = useRef<HTMLDivElement>(null)
+
+	const toggleTheme = useCallback((e: MouseEvent) => {
+		document.documentElement.classList.toggle('dark')
+		document.documentElement.style.colorScheme =
+			document.documentElement.classList.contains('dark') ? 'dark' : 'light'
+	}, [])
+
+	useEffect(() => {
+		toggleThemeRef.current?.addEventListener('click', toggleTheme)
+
+		return () => {
+			toggleThemeRef.current?.removeEventListener('click', toggleTheme)
+		}
+	}, [toggleTheme])
+
+	return (
+		<div
+			ref={toggleThemeRef}
+			className="rounded-full p-2 bg-gray-300 hover:bg-gray-200 cursor-pointer border border-gray-400/50 dark:bg-gray-600 dark:hover:bg-gray-500"
+		>
+			<Moon className="size-4 hidden dark:block" />
+			<Sun className="size-4 block dark:hidden" />
+		</div>
+	)
+}
