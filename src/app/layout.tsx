a6703cb98fbc6ba66c7f4a5d86bff669933b9fa5
Cristian-F-M <cfmorales.diaz@gmail.com>
2025-04-17 10:55:01 -0500

    [style]: Add style for dark them in body element

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,7 +17,7 @@ export default function RootLayout({
 			<head>
 				<link rel="icon" type="image/svg+xml" href="./logo-sena.svg" />
 			</head>
-			<body>{children}</body>
+			<body className="dark:bg-gray-900">{children}</body>
 		</html>
 	)
 }
