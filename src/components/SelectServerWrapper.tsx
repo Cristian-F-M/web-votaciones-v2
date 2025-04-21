241732b68fa181f42188448e3bc56e8cc5c9d26e
Cristian-F-M <cfmorales.diaz@gmail.com>
2025-04-21 15:25:36 -0500

    fix: Handle error case in SelectWithData by invoking onErrorChange

--- src/components/SelectServerWrapper.tsx
+++ src/components/SelectServerWrapper.tsx
@@ -5,9 +5,16 @@ import type { InputSelectWrapperProps } from '@/types/input'
 async function SelectWithData({
 	dataKey,
 	items,
+	onErrorChange,
 	...props
 }: InputSelectWrapperProps) {
 	const resolvedItems = await Promise.resolve(items())
+
+	if (!resolvedItems.ok) {
+		if (onErrorChange) onErrorChange('No se pudo obtener los datos')
+		return <Select {...props} label={props.label} items={[]} />
+	}
+
 	const newItems = dataKey ? resolvedItems[dataKey] : []
 	return <Select {...props} items={newItems} />
 }
