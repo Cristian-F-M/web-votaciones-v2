23bea07f5f83b0b09527fb52080e15842a443f98
Cristian-F-M <cfmorales.diaz@gmail.com>
2025-04-21 11:15:17 -0500

    refactor: Simplify SelectServerWrapper by extracting async logic to SelectWithData

--- src/components/SelectServerWrapper.tsx
+++ src/components/SelectServerWrapper.tsx
@@ -2,18 +2,20 @@ import { Suspense } from 'react'
 import { Select } from './Select'
 import type { InputSelectWrapperProps } from '@/types/input'
 
-export async function SelectServerWrapper<T>({
-	items,
+async function SelectWithData({
 	dataKey,
+	items,
 	...props
 }: InputSelectWrapperProps) {
+	const resolvedItems = await Promise.resolve(items())
+	const newItems = dataKey ? resolvedItems[dataKey] : []
+	return <Select {...props} items={newItems} />
+}
+
+export function SelectServerWrapper(props: InputSelectWrapperProps) {
 	return (
 		<Suspense fallback={<Select {...props} label="Cargando..." items={[]} />}>
-			{(async () => {
-				const resolvedItems = await Promise.resolve(items())
-				const newItems = dataKey ? resolvedItems[dataKey] : []
-				return <Select {...props} items={newItems} />
-			})()}
+			<SelectWithData {...props} />
 		</Suspense>
 	)
 }
