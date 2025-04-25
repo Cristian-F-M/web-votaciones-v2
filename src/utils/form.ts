5750de0b44775277a9c2663179b19d3b6e5e9295
Cristian-F-M <cfmorales.diaz@gmail.com>
2025-04-25 12:16:08 -0500

    chore: Add getProvessedErrors function to get simplify errors got from api

--- src/utils/form.ts
+++ src/utils/form.ts
@@ -1,3 +1,18 @@
+import type { Errors } from '@/types/api'
+
 export function getErrorEntries(errors: Record<string, any>) {
 	return Object.entries(errors).filter(([_, value]) => value)
+}
+
+export function getProcessedErrors(
+	errors: Errors
+): Record<string, string | null> {
+	const errorsEntries = Object.entries(errors)
+	const locallyErrors: Record<string, string | null> = {}
+
+	for (const [_, error] of errorsEntries) {
+		const { path, msg } = error[0]
+		locallyErrors[path] = msg
+	}
+	return locallyErrors
 }
