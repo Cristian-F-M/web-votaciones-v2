5128510f32d8b30f9763fc8a3d9d3cb138029a90
Cristian-F-M <cfmorales.diaz@gmail.com>
2025-04-30 20:36:08 -0500

    styles: Format next config file

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,11 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
-  /* config options here */
-  env: {
-    API_URL: process.env.NEXT_PUBLIC_API_URL
-  },
+	/* config options here */
+	env: {
+		API_URL: process.env.NEXT_PUBLIC_API_URL
+	},
 	allowedDevOrigins: ['192.168.0.101']
-};
+}
 
-export default nextConfig;
+export default nextConfig
