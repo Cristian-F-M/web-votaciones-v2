c35474f1122c6edcb76167ace2514e463e01f883
Cristian-F-M <cfmorales.diaz@gmail.com>
2025-04-30 20:35:52 -0500

    chore: Add allowedDevOrigin to see web from another ip

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,8 @@ const nextConfig: NextConfig = {
   /* config options here */
   env: {
     API_URL: process.env.NEXT_PUBLIC_API_URL
-  }
+  },
+	allowedDevOrigins: ['192.168.0.101']
 };
 
 export default nextConfig;
