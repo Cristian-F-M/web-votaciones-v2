8a7a913083dd781768ccf23673497f7c71ad8114
Cristian-F-M <cfmorales.diaz@gmail.com>
2025-05-01 21:02:06 -0500

    feat: Add clientLayout component to add client code | components

---
+++ src/app/clientLayout.tsx
@@ -0,0 +1,11 @@
+import { SnackbarProvider } from '@/components/SnackbarProvider'
+import ThemeScript from '@/components/ThemeScript'
+
+export default function ClientLayout() {
+	return (
+		<>
+			<ThemeScript />
+			<SnackbarProvider />
+		</>
+	)
+}
