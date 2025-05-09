1f5b5522625deed9824600fc40d9aa84c9c190ad
Cristian-F-M <cfmorales.diaz@gmail.com>
2025-05-09 17:07:30 -0500

    feat: Add validateFieldsNotEmpty functionto validate that the fields are not empty,  add EMAIL_REGEX and function to validate if is an email valid

--- src/utils/form.ts
+++ src/utils/form.ts
@@ -1,4 +1,12 @@
 import type { Errors } from '@/types/api'
+import type {
+	ValidateFieldsProps,
+	ValidateFieldsReturnType
+} from '@/types/forms'
+
+export const EMAIL_REGEX = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/
+
+export const isEmailValid = (email: string) => EMAIL_REGEX.test(email)
 
 export function getErrorEntries(errors: Record<string, any>) {
 	return Object.entries(errors).filter(([_, value]) => value)
@@ -15,4 +23,29 @@ export function getProcessedErrors(
 		locallyErrors[path] = msg
 	}
 	return locallyErrors
+}
+
+export function validateFieldsNotEmpty(
+	fields: ValidateFieldsProps
+): ValidateFieldsReturnType {
+	const errors: { [key: string]: string } = {}
+
+	for (const key in fields) {
+		if (['namedItem', 'length', 'item'].includes(key)) continue
+		if (!Number.isNaN(Number(key))) continue
+		const field = fields[key]
+		const name = field.name || key
+
+		if (!field.value || field.value.trim() === '') {
+			errors[name] = 'Este campo es obligatorio'
+		}
+
+		if (field instanceof HTMLSelectElement) {
+			if (field.value === 'default-value') {
+				errors[name] = 'Seleccione un campo de la lista'
+			}
+		}
+	}
+
+	return errors
 }
