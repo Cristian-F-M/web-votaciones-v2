aa4204c4841de3c8d14943f755ffbe3610d882a2
Cristian-F-M <cfmorales.diaz@gmail.com>
2025-05-12 18:34:16 -0500

    types: Add User, Role, TypeDocument model types

---
+++ src/types/models.d.ts
@@ -0,0 +1,42 @@
+export interface User {
+	id: string
+	name: string
+	lastname: string
+	typeDocument: string
+	document: string
+	phone: string
+	email: string
+	imageUrl?: string
+	role: string
+	voted: boolean
+	typeDocumentUser: TypeDocument
+	roleUser: Role
+}
+
+export type UserHome = Pick<
+	User,
+	| 'id'
+	| 'name'
+	| 'lastname'
+	| 'document'
+	| 'email'
+	| 'phone'
+	| 'voted'
+	| 'imageUrl'
+>
+
+export interface TypeDocument {
+	id: string
+	value: string
+	name: string
+	code: string
+	description: string
+}
+
+export interface Role {
+	id: string
+	value: string
+	name: string
+	code: string
+	description: string
+}
