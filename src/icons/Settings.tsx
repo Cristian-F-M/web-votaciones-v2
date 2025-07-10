b160c8e7177afb2c7f7f49b6651525b51dcadc93
Cristian-F-M <cfmorales.diaz@gmail.com>
2025-07-10 17:21:24 -0500

    Add icons

---
+++ src/icons/Settings.tsx
@@ -0,0 +1,21 @@
+export default function Settings(props: React.SVGProps<SVGSVGElement>) {
+	return (
+		<svg
+			xmlns="http://www.w3.org/2000/svg"
+			width="24"
+			height="24"
+			fill="none"
+			stroke="currentColor"
+			strokeLinecap="round"
+			strokeLinejoin="round"
+			strokeWidth="1.5"
+			viewBox="0 0 24 24"
+			{...props}
+		>
+			<title>settings</title>
+			<path stroke="none" d="M0 0h24v24H0z" />
+			<path d="M10.3 4.3c.5-1.7 3-1.7 3.4 0a1.7 1.7 0 0 0 2.5 1c1.6-.9 3.4 1 2.4 2.5a1.7 1.7 0 0 0 1 2.5c1.8.5 1.8 3 0 3.4a1.7 1.7 0 0 0-1 2.5c1 1.6-.8 3.4-2.4 2.4a1.7 1.7 0 0 0-2.5 1c-.5 1.8-3 1.8-3.4 0a1.7 1.7 0 0 0-2.5-1c-1.6 1-3.4-.8-2.4-2.4a1.7 1.7 0 0 0-1-2.5c-1.8-.5-1.8-3 0-3.4a1.7 1.7 0 0 0 1-2.5c-1-1.6.8-3.4 2.4-2.4 1 .6 2.2 0 2.5-1z" />
+			<path d="M9 12a3 3 0 1 0 6 0 3 3 0 0 0-6 0" />
+		</svg>
+	)
+}
