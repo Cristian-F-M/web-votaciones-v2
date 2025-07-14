887a4ab833211e025a0959d7cfacab0c3b29a691
Cristian-F-M <cfmorales.diaz@gmail.com>
2025-07-14 16:19:00 -0500

    Add styles when dark mode is active and remove useless border

--- src/components/candidate/CandidateCardFallback.tsx
+++ src/components/candidate/CandidateCardFallback.tsx
@@ -1,14 +1,14 @@
 export function CandidateCardFallback() {
 	return (
 		<div className="w-[260px] h-[380px] border border-gray-400/60 rounded-[15px] shadow py-3 animate-pulse">
-			<div className="w-11/12 mx-auto h-[230px] border-b border-gray-300 rounded bg-gray-300" />
+			<div className="w-11/12 mx-auto h-[230px] rounded bg-gray-300 dark:bg-gray-700" />
 
-			<div className="w-40 h-4 bg-gray-300 mx-auto mt-4" />
+			<div className="w-40 h-4 bg-gray-300 dark:bg-gray-700 mx-auto mt-4" />
 			<div className="w-fit mx-auto mt-2">
-				<span className="block w-48 h-2 bg-gray-300 " />
-				<span className="block w-32 h-2 bg-gray-300 mt-px mx-auto" />
+				<span className="block w-48 h-2 bg-gray-300 dark:bg-gray-700 " />
+				<span className="block w-32 h-2 bg-gray-300 dark:bg-gray-700 mt-px mx-auto" />
 			</div>
-			<div className="w-11/12 mx-auto h-9 bg-gray-300 mt-7 rounded" />
+			<div className="w-11/12 mx-auto h-9 bg-gray-300 dark:bg-gray-700 mt-7 rounded" />
 		</div>
 	)
 }
