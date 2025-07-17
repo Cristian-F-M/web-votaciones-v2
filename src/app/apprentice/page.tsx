96be6ba4ca6fa5970b1b04db7181ba528cc5719e
Cristian-F-M <cfmorales.diaz@gmail.com>
2025-07-17 17:51:36 -0500

    Add vote section and loading  for candidates

--- src/app/apprentice/page.tsx
+++ src/app/apprentice/page.tsx
@@ -3,14 +3,42 @@ import { useUser } from '@/states/useUser'
 import '@/styles/apprentice.css'
 import Clock from '@/icons/Clock'
 import { useRemainingTime } from '@/hooks/useRemainingTime'
-import { useMemo } from 'react'
+import { useEffect, useMemo, useState } from 'react'
 import { SingleTimeCard } from '@/components/SingleTimeCard'
 import AlertIcon from '@/icons/Alert'
+import type { Candidate } from '@/types/models'
+import { doFetch } from '@/utils/fetch'
+import type { GetCandidatesResponse } from '@/types/api'
+import { CandidateCard } from '@/components/candidate/CandidateCard'
+import { CandidateCardFallback } from '@/components/candidate/CandidateCardFallback'
+import { enqueueSnackbar } from 'notistack'
 
 export default function IndexPage() {
 	const user = useUser((state) => state.user)
 	const targetDate = useMemo(() => new Date(1755710169727), [])
 	const { remainingTime } = useRemainingTime(targetDate)
+	const [candidates, setCandidates] = useState<Candidate[] | null>(null)
+
+	useEffect(() => {
+		async function getCandidates() {
+			const res = await doFetch<GetCandidatesResponse>({
+				url: '/candidate/all'
+			})
+
+			if (!res.ok) {
+				enqueueSnackbar(res.message, {
+					variant: 'error',
+					autoHideDuration: 3000,
+					preventDuplicate: true
+				})
+				return
+			}
+
+			setCandidates(res.candidates)
+		}
+
+		getCandidates()
+	}, [])
 
 	return (
 		<>
@@ -35,6 +63,25 @@ export default function IndexPage() {
 						<span className="time-separator">:</span>
 						<SingleTimeCard label="seg" value={remainingTime.seconds} />
 					</div>
+				</div>
+
+				<div className="[&_svg]:size-5 dark:[&_svg]:text-yellow-400 [&_svg]:text-red-700 w-fit mx-auto mt-10 flex flex-row gap-1.5 items-center ">
+					<AlertIcon />
+					<span className="block">Solo puedes votar una vez</span>
+				</div>
+
+				<div className="candidates w-10/12 mx-auto mt-6 flex flex-row gap-6 flex-wrap justify-center">
+					{candidates &&
+						candidates.length > 0 &&
+						candidates.map((candidate) => (
+							<CandidateCard key={candidate.id} candidate={candidate} />
+						))}
+
+					{(!candidates || candidates.length <= 0) &&
+						Array.from({ length: 4 }).map((_, index) => (
+							// biome-ignore lint/suspicious/noArrayIndexKey: <explanation>
+							<CandidateCardFallback key={index} />
+						))}
 				</div>
 			</main>
 		</>
