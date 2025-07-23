d017f751395289f14e9521136e59f13b940befac
Cristian-F-M <cfmorales.diaz@gmail.com>
2025-07-23 16:57:16 -0500

    Add icons

---
+++ src/icons/Calendar.tsx
@@ -0,0 +1,20 @@
+export default function Calendar(props: React.SVGProps<SVGSVGElement>) {
+	return (
+		<svg
+			xmlns="http://www.w3.org/2000/svg"
+			width="24"
+			height="24"
+			fill="none"
+			stroke="currentColor"
+			strokeLinecap="round"
+			strokeLinejoin="round"
+			strokeWidth="1.5"
+			viewBox="0 0 24 24"
+			{...props}
+		>
+			<title>Calendar</title>
+			<path stroke="none" d="M0 0h24v24H0z" />
+			<path d="M4 7a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H6a2 2 0 0 1-2-2V7zM16 3v4M8 3v4M4 11h16M11 15h1M12 15v3" />
+		</svg>
+	)
+}
---
+++ src/icons/Mobile.tsx
@@ -0,0 +1,20 @@
+export default function Mobile(props: React.SVGProps<SVGSVGElement>) {
+	return (
+		<svg
+			xmlns="http://www.w3.org/2000/svg"
+			width="24"
+			height="24"
+			fill="none"
+			stroke="currentColor"
+			strokeLinecap="round"
+			strokeLinejoin="round"
+			strokeWidth="1.5"
+			viewBox="0 0 24 24"
+			{...props}
+		>
+			<title>Device mobile</title>
+			<path stroke="none" d="M0 0h24v24H0z" />
+			<path d="M6 5a2 2 0 0 1 2-2h8a2 2 0 0 1 2 2v14a2 2 0 0 1-2 2H8a2 2 0 0 1-2-2V5zM11 4h2M12 17v0" />
+		</svg>
+	)
+}
