5ed587410270736a7300259e6a6148026df803c8
Cristian-F-M <cfmorales.diaz@gmail.com>
2025-07-25 11:06:24 -0500

    Use modal component instead of simple html

--- src/components/apprentice/NoVote.tsx
+++ src/components/apprentice/NoVote.tsx
@@ -1,22 +1,13 @@
 import Calendar from '@/icons/Calendar'
 import Bell from '@/icons/Bell'
 import Mobile from '@/icons/Mobile'
-import { useCallback, useState } from 'react'
+import { useRef } from 'react'
 import { useUser } from '@/states/useUser'
+import { ActivateNotificationsModal } from './ActivateNotificationsModal'
 
 export function NoVote() {
 	const user = useUser((state) => state.user)
-	const maskedEmail = user?.email
-		? (() => {
-				const [username, domain] = user.email.split('@')
-				return `${username.slice(0, 2)}****@${domain}`
-			})()
-		: ''
-
-	const handleModal = useCallback((type: 'close' | 'open') => {
-		const action = type === 'open' ? 'showModal' : 'close'
-		document.querySelector('dialog')?.[action]()
-	}, [])
+	const buttonTriggerRef = useRef<HTMLButtonElement>(null)
 
 	return (
 		<>
@@ -43,7 +34,7 @@ export function NoVote() {
 						<button
 							type="button"
 							className="bg-(--color) hover:bg-(--color)/80 border-none active:bg-(--color)/60 active:dark:bg-(--color)/60"
-							onClick={() => handleModal('open')}
+							ref={buttonTriggerRef}
 						>
 							<Bell />
 							Activar notificaciones
@@ -69,76 +60,7 @@ export function NoVote() {
 				</div>
 			</div>
 
-			<style>{`
-      dialog::backdrop {
-        background-color: rgba(0, 0, 0, 0.5);
-        z-index: 10;
-        display: flex;
-        flex-direction: column;
-        justify-content: center;
-        align-items: center;
-      }
-
-        dialog[open] {
-          display: flex;
-          scale: 1;
-          opacity: 1;
-
-
-          @starting-style {
-            scale: 0.8;
-            opacity: 0;
-          }
-        }
-
-
-        dialog {
-          display: none;
-          scale: 0.8;
-          opacity: 0;
-          transition: all 0.3s allow-discrete;
-        }
-
-    `}</style>
-
-			<dialog className="m-auto rounded shadow flex-col p-5 w-11/12 md:max-w-[450px]">
-				<header className="flex flex-row gap-1 items-center ">
-					<Bell className="text-(--color) size-5" />
-					<h3 className="md:text-base text-sm font-semibold">
-						Activar notificaciones por email
-					</h3>
-				</header>
-				<main className="mt-px">
-					<p className="text-gray-700 md:text-sm text-xs dark:text-gray-400">
-						Se enviará un correo electrónico a{' '}
-						<span className="text-(--color) font-semibold">{maskedEmail}</span>{' '}
-						cuando haya nuevas votaciones
-					</p>
-
-					<div className="bg-(--color)/20 px-3 py-2 rounded mt-3 border border-(--color)/30 text-xs">
-						<p>
-							📧 Recibiras una notificación inmediata cuando se abran nuevos
-							procesos de votacion.
-						</p>
-					</div>
-
-					<div className="mt-3 flex md:flex-row flex-col-reverse gap-2 justify-end [&_button]:px-3 [&_button]:py-1.5 [&_button]:rounded [&_button]:text-sm [&_button]:cursor-pointer">
-						<button
-							className="border border-gray-300 hover:bg-gray-300/30 dark:border-gray-600 dark:hover:bg-gray-600/30 active:bg-gray-300/30 dark:active:bg-gray-600/30"
-							type="button"
-							onClick={() => handleModal('close')}
-						>
-							Cancelar
-						</button>
-						<button
-							className="bg-(--color) hover:bg-(--color)/80 dark:bg-(--color)/80 dark:hover:bg-(--color)/60 active:bg-(--color)/60 dark:active:bg-(--color)/60"
-							type="button"
-						>
-							Activar notificaciones
-						</button>
-					</div>
-				</main>
-			</dialog>
+			<ActivateNotificationsModal buttonTriggerRef={buttonTriggerRef} />
 		</>
 	)
 }
