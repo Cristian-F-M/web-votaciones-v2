ed6d37022be486b5f4fbcb58487b38ee1d8bc10d
Cristian-F-M <cfmorales.diaz@gmail.com>
2025-10-29 16:27:22 -0500

    Add function to show message with default options

--- src/utils/dom.ts
+++ src/utils/dom.ts
@@ -1,3 +1,6 @@
+import { snackbarOptions } from '@/constants/snackbar'
+import { type OptionsObject, enqueueSnackbar } from 'notistack'
+
 export function scrollSmooth(element: HTMLElement | string) {
 	let newElement: ReturnType<Document['querySelector']> | HTMLElement | null =
 		null
@@ -13,4 +16,18 @@ export function getStringTime(seconds: number) {
 	const minutes = Math.floor(seconds / 60)
 	const remainingSeconds = seconds % 60
 	return `${minutes}:${remainingSeconds < 10 ? '0' : ''}${remainingSeconds}`
+}
+
+interface SnackbarProps {
+	message: string
+	variant?: OptionsObject['variant']
+	options?: OptionsObject
+}
+
+export function snackbar({
+	message,
+	variant = 'info',
+	options = snackbarOptions
+}: SnackbarProps) {
+	return enqueueSnackbar(message, { variant, ...options })
 }
