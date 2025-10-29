35f965fba5d87e420a7e63d33f9b0c398d084134
Cristian-F-M <cfmorales.diaz@gmail.com>
2025-10-29 16:26:57 -0500

    Add snackbar options as constant

---
+++ src/constants/snackbar.ts
@@ -0,0 +1,8 @@
+import { enqueueSnackbar, type OptionsObject } from 'notistack'
+
+export const snackbarOptions: OptionsObject = {
+	variant: 'info',
+	autoHideDuration: 2000,
+	preventDuplicate: true
+}
+
