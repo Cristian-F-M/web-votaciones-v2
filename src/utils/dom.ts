6a86593fd41d96e00486e52d668c0c2d57047b02
Cristian-F-M <cfmorales.diaz@gmail.com>
2025-10-29 16:19:11 -0500

    Add function to get string time (like mm:ss)

--- src/utils/dom.ts
+++ src/utils/dom.ts
@@ -8,3 +8,9 @@ export function scrollSmooth(element: HTMLElement | string) {
 		behavior: 'smooth'
 	})
 }
+
+export function getStringTime(seconds: number) {
+	const minutes = Math.floor(seconds / 60)
+	const remainingSeconds = seconds % 60
+	return `${minutes}:${remainingSeconds < 10 ? '0' : ''}${remainingSeconds}`
+}
