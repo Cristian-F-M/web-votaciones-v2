a345551b37a5bb69835a18b073cdf1606b00ed90
Cristian-F-M <cfmorales.diaz@gmail.com>
2025-11-14 16:45:13 -0500

    Add CodeInput component

---
+++ src/components/CodeInput.tsx
@@ -0,0 +1,90 @@
+'use client'
+import { useCallback, useEffect, useRef, useState } from 'react'
+import { twMerge } from 'tailwind-merge'
+import { SingleCodeInput } from './SingleCodeInput'
+
+type ControlledProps = {
+	value: string
+	setValue: React.Dispatch<React.SetStateAction<string>>
+}
+
+type UncontrolledProps = {
+	name: string
+	id: string
+}
+
+type CodeInputProps = {
+	size: number
+} & (ControlledProps | UncontrolledProps) &
+	React.InputHTMLAttributes<HTMLInputElement>
+
+export function CodeInput({ size, className, ...props }: CodeInputProps) {
+	const [code, setCode] = useState(''.padEnd(size, ' '))
+	const isControled = 'value' in props && 'setValue' in props
+	const inputRefs = useRef<HTMLInputElement[]>([])
+
+	const handleChange = useCallback(
+		(text: string, index?: number) => {
+			let fn = setCode
+			if (isControled) fn = props.setValue
+
+			if (index === undefined) {
+				fn(text)
+				text.split('').forEach((char, i) => {
+					const input = inputRefs.current[i]
+
+					if (input) input.value = char
+				})
+
+				inputRefs.current.at(-1)?.focus()
+				return
+			}
+
+			fn((prev) => {
+				const chars = prev.split('')
+				chars[index] = text
+
+				return chars.join('')
+			})
+		},
+		// biome-ignore lint/correctness/useExhaustiveDependencies: <explanation>
+		[isControled, props]
+	)
+
+	const inputName = !isControled ? props.name : 'input-code'
+
+	useEffect(() => {
+		inputRefs.current.at(0)?.focus()
+	}, [])
+
+	return (
+		<div className={twMerge('w-fit', className)}>
+			<input
+				type="text"
+				name={inputName}
+				id="input-code--input-result"
+				className="hidden uppercase"
+				disabled
+				value={code}
+				onChange={() => {}}
+				readOnly
+				{...props}
+			/>
+			<div className="flex gap-3">
+				{Array.from({ length: size }).map((_, index) => (
+					<SingleCodeInput
+						// biome-ignore lint/suspicious/noArrayIndexKey: It does not matter
+						key={`sngle-code-input-${index}`}
+						onChange={handleChange}
+						index={index}
+						size={size}
+						inputRef={(el) => {
+							if (!el) return
+							inputRefs.current[index] = el
+						}}
+					/>
+				))}
+			</div>
+		</div>
+	)
+}
---
+++ src/components/SingleCodeInput.tsx
@@ -0,0 +1,80 @@
+import { useCallback } from "react"
+
+type SingleCodeInputProps = {
+	onChange: (newChar: string, index?: number) => void
+	index: number
+	size: number
+	inputRef: (el: HTMLInputElement | null) => void
+}
+
+export function SingleCodeInput({
+	onChange,
+	index,
+	size,
+	inputRef
+}: SingleCodeInputProps) {
+	const regex = /^[a-zA-Z0-9]$/
+	const pasteRegex = new RegExp(`^[a-zA-Z0-9]{${size}}$`)
+
+	const handleKeyDown = useCallback(
+		(event: React.KeyboardEvent<HTMLInputElement>) => {
+			const { key, ctrlKey } = event
+			const $input = event.currentTarget
+			const value = $input.value.trim()
+			const isBackspace = key === 'Backspace'
+			const isDelete = key === 'Delete'
+
+			if (ctrlKey && key === 'v') return
+
+			if (isBackspace || isDelete) {
+				if (isBackspace && value.length === 0) {
+					const $prevInput = $input.previousElementSibling as HTMLInputElement
+					if ($prevInput) $prevInput.focus()
+				}
+
+				onChange(' ', index)
+				return
+			}
+
+			if (regex.test(key)) {
+				if (value.length >= 1) {
+					const $nextInput = $input.nextElementSibling as HTMLInputElement
+					if ($nextInput) $nextInput.focus()
+					return
+				}
+
+				onChange(key, index)
+				setTimeout(() => {
+					const $nextInput = $input.nextElementSibling as HTMLInputElement
+					if ($nextInput) $nextInput.focus()
+				}, 0)
+			}
+		},
+		[index, onChange]
+	)
+
+	const handlePaste = useCallback(
+		(event: React.ClipboardEvent<HTMLInputElement>) => {
+			event.preventDefault()
+
+			const { clipboardData } = event
+			const text = clipboardData.getData('text/plain')
+
+			if (!pasteRegex.test(text)) return
+
+			onChange(text)
+		},
+		[pasteRegex, onChange]
+	)
+
+	return (
+		<input
+			className="aspect-[1/1.3] w-12 bg-gray-300 dark:bg-gray-900 border border-gray-400/60 dark:border-gray-700/80 rounded flex items-center justify-center text-black dark:text-white text-lg text-center uppercase focus:outline-1.5 outline-primary dark:outline-primary caret-black dark:caret-white"
+			type="text"
+			maxLength={1}
+			onKeyDown={handleKeyDown}
+			ref={inputRef}
+			onPaste={handlePaste}
+		/>
+	)
+}
