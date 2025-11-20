018be96e7cb2ccb05a9e324871a4b57aba43e4a3
Cristian-F-M <cfmorales.diaz@gmail.com>
2025-11-19 19:11:09 -0500

    Add function to user state to get and save user object

--- src/states/useUser.ts
+++ src/states/useUser.ts
@@ -1,12 +1,22 @@
+import type { UserHomeResponse } from '@/types/api'
 import type { UserHome } from '@/types/models'
+import { doFetch } from '@/utils/fetch'
 import { create } from 'zustand'
 
 interface UserState {
 	user: UserHome | null
 	setUser: (user: UserState['user']) => void
+	getUser: () => Promise<UserHome | null>
 }
 
 export const useUser = create<UserState>()((set) => ({
 	user: null,
-	setUser: (user) => set({ user })
+	setUser: (user) => set({ user }),
+	getUser: getUser
 }))
+
+async function getUser() {
+	const { ok, ...data } = await doFetch<UserHomeResponse>({ url: '/user' })
+	if (!ok) return null
+	if ('user' in data) return data.user
+}
