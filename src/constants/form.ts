8d4638311c8992e5462618e5363eaa6d4401fa4f
Cristian-F-M <cfmorales.diaz@gmail.com>
2025-11-24 15:52:25 -0500

    add input validation constant

--- src/constants/form.ts
+++ src/constants/form.ts
@@ -5,3 +5,19 @@ export const PASSWORD_REGEX = new RegExp(
 	`^(?=(.*[A-Z]))(?=(.*[a-z]))(?=(.*\d))(?=(.*[\W_]))[A-Za-z\d\W_]{${PASSWORD_MIN_MAX_LENGHT[0]},${PASSWORD_MIN_MAX_LENGHT[1]}}$`
 )
 
+export const INPUTS_VALIDATIONS = {
+	requiredMessage: 'Este campo es obligatorio',
+	document: {
+		min: 5,
+		max: 15,
+		sizeMessage: 'El documento debe tener entre 5 y 15 caracteres'
+	},
+	password: {
+		sizeMessage: 'La contraseña debe tener entre 8 y 20 caracteres',
+		strongMessage:
+			'La contraseña debe tener al menos 1 mayúscula, 1 minúscula, 1 número y 1 símbolo especial'
+	},
+	select: {
+		requiredMessage: 'Selecciona una opción de la lista'
+	}
+}
