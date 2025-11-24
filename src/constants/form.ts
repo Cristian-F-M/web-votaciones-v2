e977d9473c234337e2a5521e6ad48e439b859de6
Cristian-F-M <cfmorales.diaz@gmail.com>
2025-11-24 15:22:58 -0500

    Add form constants

--- src/constants/form.ts
+++ src/constants/form.ts
@@ -1,1 +1,7 @@
-export const SELECT_DEFAULT_VALUE = "default-value"
+export const SELECT_DEFAULT_VALUE = 'default-value'
+export const PASSWORD_MIN_MAX_LENGHT = [8, 20]
+export const EMAIL_REGEX = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/
+export const PASSWORD_REGEX = new RegExp(
+	`^(?=(.*[A-Z]))(?=(.*[a-z]))(?=(.*\d))(?=(.*[\W_]))[A-Za-z\d\W_]{${PASSWORD_MIN_MAX_LENGHT[0]},${PASSWORD_MIN_MAX_LENGHT[1]}}$`
+)
+
