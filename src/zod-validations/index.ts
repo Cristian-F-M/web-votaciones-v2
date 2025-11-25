891fd64726a149aa61b2aa82fd2a4e899736960c
Cristian-F-M <cfmorales.diaz@gmail.com>
2025-11-24 19:44:39 -0500

    Add zod schemes and use them in login scheme

---
+++ src/zod-validations/index.ts
@@ -0,0 +1,31 @@
+import {
+	SELECT_DEFAULT_VALUE,
+	INPUTS_VALIDATIONS as IV,
+	PASSWORD_REGEX,
+	PASSWORD_MIN_MAX_LENGHT
+} from '@/constants/form'
+import * as z from 'zod'
+
+export const SCHEMES = {
+	typeDocument: z
+		.string()
+		.nonempty(IV.requiredMessage)
+		.refine((v) => v !== SELECT_DEFAULT_VALUE, IV.select.requiredMessage),
+	document: z
+		.string()
+		.nonempty(IV.requiredMessage)
+		.min(IV.document.min, IV.document.sizeMessage)
+		.max(IV.document.max, IV.document.sizeMessage),
+	password: z
+		.string()
+		.nonempty(IV.requiredMessage)
+		.min(PASSWORD_MIN_MAX_LENGHT[0], IV.password.sizeMessage)
+		.max(PASSWORD_MIN_MAX_LENGHT[1], IV.password.sizeMessage)
+}
+
+export const LOGIN_SCHEME = z.object({
+	typeDocument: SCHEMES.typeDocument,
+	document: SCHEMES.document,
+	password: SCHEMES.password
+})
+
