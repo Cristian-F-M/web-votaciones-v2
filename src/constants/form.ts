f9a391e65e720b0d9a7ad7b282bf4375e10d3a7d
Cristian-F-M <cfmorales.diaz@gmail.com>
2025-11-25 18:09:48 -0500

    Fix password regex adding double / for digits validation

--- src/constants/form.ts
+++ src/constants/form.ts
@@ -2,7 +2,7 @@ export const SELECT_DEFAULT_VALUE = 'default-value'
 export const PASSWORD_MIN_MAX_LENGHT = [8, 20]
 export const EMAIL_REGEX = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/
 export const PASSWORD_REGEX = new RegExp(
-	`^(?=(.*[A-Z]))(?=(.*[a-z]))(?=(.*\d))(?=(.*[\W_]))[A-Za-z\d\W_]{${PASSWORD_MIN_MAX_LENGHT[0]},${PASSWORD_MIN_MAX_LENGHT[1]}}$`
+	`^(?=.*[A-Z])(?=.*[a-z])(?=.*\\d)(?=.*[^A-Za-z0-9]).{${PASSWORD_MIN_MAX_LENGHT[0]},${PASSWORD_MIN_MAX_LENGHT[1]}}$`
 )
 
 export const INPUTS_VALIDATIONS = {
