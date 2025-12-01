05a4ff31919b7c7ca6922c486150092dfa55651d
Cristian-F-M <cfmorales.diaz@gmail.com>
2025-12-01 17:29:56 -0500

    Add dark mode to component

--- src/components/item-list/SingleItemList.tsx
+++ src/components/item-list/SingleItemList.tsx
@@ -15,7 +15,7 @@ export function SingleItemList({
 	removeItemProps?: React.ButtonHTMLAttributes<HTMLButtonElement>
 } & React.InputHTMLAttributes<HTMLInputElement>) {
 	return (
-		<div className="flex flex-row justify-between items-center bg-gray-100 rounded border border-gray-300/80  text-pretty">
+		<div className="flex flex-row justify-between items-center bg-gray-100 rounded border border-gray-300/80  text-pretty dark:bg-page-dark-bg-contrast dark:border-gray-700">
 			<input
 				className={twMerge('w-full h-[stretch] px-3 py-2', className)}
 				defaultValue={item.text}
@@ -26,7 +26,7 @@ export function SingleItemList({
 			<button
 				{...removeItemProps}
 				className={twMerge(
-					'mr-1 text-red-700 hover:bg-red-200 [&_svg]:size-4 cursor-pointer transition-colors p-0.5 rounded',
+					'mr-1 text-red-700 hover:bg-red-200 [&_svg]:size-4 cursor-pointer transition-colors p-0.5 rounded dark:text-red-500 dark:hover:bg-red-400/20',
 					removeItemProps?.className
 				)}
 				type="button"
