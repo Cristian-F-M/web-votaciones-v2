dae86cde3d629e25dddf95f68d193fc4cd779032
Cristian-F-M <cfmorales.diaz@gmail.com>
2025-12-20 17:04:46 -0500

    Update colors to use a tailwind variable

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -33,23 +33,23 @@ export default function Home() {
 	}, [verifySession])
 
 	return (
-		<div className="min-w-screen min-h-screen bg-(--color)/15 flex items-center justify-center">
+		<div className="min-w-screen min-h-screen bg-primary/15 flex items-center justify-center">
 			<main
-				className="text-(--color) w-10/12 md:max-w-92 flex flex-col items-center bg-white shadow-md 
+				className="text-primary w-10/12 md:max-w-92 flex flex-col items-center bg-white shadow-md 
       px-3 py-6 rounded-md animate-fadeIn"
 			>
 				<LogoSena className="size-24" />
 
 				<h1 className="flex flex-col items-center text-3xl font-semibold leading-7 mt-6 text-black">
 					Votaciones
-					<span className="text-base text-(--color)/80">CGAO</span>
+					<span className="text-base text-primary/80">CGAO</span>
 				</h1>
 
 				<p className="text-gray-600 text-sm md:text-base text-center px-3 mt-3">
 					Estamos verificando tu sesión. Un momento por favor...
 				</p>
 
-				<Loader className="text-(--color) size-10 mt-4" />
+				<Loader className="text-primary size-10 mt-4" />
 			</main>
 		</div>
 	)
