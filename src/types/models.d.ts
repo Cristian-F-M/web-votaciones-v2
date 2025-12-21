6312c1db64e032439f941e1c80e0cf91b40c6e98
Cristian-F-M <cfmorales.diaz@gmail.com>
2025-12-20 19:43:11 -0500

    Update models types

--- src/types/models.d.ts
+++ src/types/models.d.ts
@@ -1,77 +1,170 @@
-export interface User {
+import type { AllowedSessionType, ConfigScope } from '.'
+
+export interface Candidate {
 	id: string
-	name: string
-	lastname: string
-	typeDocument: string
-	document: string
-	phone: string
-	email: string
-	imageUrl?: string
-	role: string
-	voted: boolean
-	votedCandidateId: Candidate['id']
-	typeDocumentUser: TypeDocument
-	roleUser: Role
+	userId: string
+	description: string
+	isActive: boolean
+	createdAt?: Date
+	updatedAt?: Date
+
+	votes: Vote[]
+	objectives: Objective[]
+	user: User
 }
 
-export type UserHome = Pick<
-	User,
-	| 'id'
-	| 'name'
-	| 'lastname'
-	| 'document'
-	| 'email'
-	| 'phone'
-	| 'voted'
-	| 'imageUrl'
-	| 'typeDocumentUser'
-	| 'roleUser'
-	| 'votedCandidateId'
->
-
-export interface TypeDocument {
+export interface Config {
 	id: string
-	value: string
 	name: string
 	code: string
 	description: string
+	value: string
+	scope: ConfigScope
+	createdAt?: Date
+	updatedAt?: Date
 }
 
 export interface Role {
 	id: string
-	value: string
 	name: string
 	code: string
 	description: string
+	createdAt?: Date
+	updatedAt?: Date
+
+	users: User[]
 }
 
-export interface Candidate {
+export interface Session {
 	id: string
-	userId: Pick<User, 'id'>
-	imageUrl: string
+	token: string
+	expires: Date
+	userId: string
+	type: AllowedSessionType
+
+	user: User
+}
+
+export interface TypeDocument {
+	id: string
+	name: string
+	code: string
 	description: string
-	votes: number
-  objectives: Objective[]
-	user: Pick<User, 'id' | 'name' | 'lastname' | 'document' | 'email'>
+	createdAt?: Date
+	updatedAt?: Date
+
+	users: User[]
+}
+
+export interface User {
+	id: string
+	typeDocumentId: string
+	document: string
+	email: string
+	shiftTypeId: string
+	roleId: string
+	createdAt?: Date
+	updatedAt?: Date
+
+	typeDocument: TypeDocument
+	votes: Vote[]
+	deviceTokens: DeviceToken[]
+	role: Role
+	passwordResets: PasswordReset[]
+	sessions: Session[]
+	profile: Profile
+	candidate?: Candidate
+	shiftType: ShiftType
+}
+
+export interface PasswordReset {
+	id: string
+	userId: string
+	attemps: number
+	isActive: boolean
+	nextSendAt: Date | null | undefined
+	expiresAt: Date | null | undefined
+	usedAt: Date | null | undefined
+	createdAt?: Date
+	updatedAt?: Date
+
+	user: User
+}
+
+export interface DeviceToken {
+	id: string
+	userId: string
+	deviceType: 'ios' | 'android' | 'web'
+	lastUsedAt: Date | null | undefined
+	isActive: boolean
+	createdAt?: Date
+	updatedAt?: Date
+
+	user: User
 }
 
 export interface Vote {
 	id: string
-	userId: Pick<User, 'id'>
-	cantVotes: number
+	userId: string
+	candidateId: string
+	electionId: string
+	createdAt?: Date
+	updatedAt?: Date
+
+	candidate: Candidate
+	user: User
+}
+
+export interface Election {
+	id: string
+	apprenticeCount: number
 	totalVotes: number
-	startDate: string
-	endDate: string
-	finishVoteInfo: {
-		cantApprentices: number
-		totalVotes: number
-		cantVotesWinner: number
-		candidates: Candidate[]
-	}
-	isFinished: boolean
+	winnerVoteCount: number
+	winner: Candidate
+	startDate: Date
+	endDate: Date
+	status: 'active' | 'finished' | 'canceled'
+	shiftTypeId: string
+	createdAt?: Date
+	updatedAt?: Date
+
+	votes: Vote[]
+	shiftType: ShiftType
 }
 
 export interface Objective {
-  id: string
-  text: string
+	id: string
+	text: string
+	candidateId: string
+	createdAt?: Date
+	updatedAt?: Date
+
+	candidate: Candidate
+}
+
+export interface ShiftType {
+	id: string
+	name: string
+	code: string
+	description: string
+	startTime: string
+	endTime: string
+	createdAt?: Date
+	updatedAt?: Date
+
+	elections: Election[]
+	users: User[]
+}
+
+export interface Profile {
+	id: string
+	userId: string
+	name: string
+	lastname: string
+	phone: string
+	imageUrl: string | undefined | null
+	createdAt?: Date
+	updatedAt?: Date
+
+	user: User
 }
