4a71b44a47f7cabd0d83ad88fe6fbbc58067aeaa
Cristian-F-M <cfmorales.diaz@gmail.com>
2025-12-21 10:31:06 -0500

    Update api response types

--- src/types/api.d.ts
+++ src/types/api.d.ts
@@ -1,6 +1,6 @@
 import type { LoginErrors, RegisterErrors } from './forms'
 import type { SelectItem } from './input'
-import type { TypeDocument, Role } from './models'
+import type { TypeDocument, Role, Config, Profile } from './models'
 
 export type HttpMethod = 'GET' | 'POST' | 'PUT' | 'DELETE' | 'PATCH'
 
@@ -17,188 +17,291 @@ export interface FetchProps {
 	cache?: RequestInit['cache']
 }
 
-export type FetchSuccess<T> = {
-	ok: true
+export interface FormError {
+	[key: string]: { path: string; msg: string }[]
 }
 
-export type FetchError = {
-	ok: false
+export interface DatabaseError {
 	message: string
-}
-
-export interface GetTypeDocumentsResponse {
-	ok: boolean
-	message?: string
-	typesDocuments: TypeDocument[]
-}
-
-export interface LoginSuccesResponse {
-	ok: true
-	message: string
-	urlRedirect: string
-	token?: string
-}
-
-export interface FieldError {
-	type: string
-	msg: string
 	path: string
-	location: string
-	message?: string
+	type: string
 }
 
-export interface Errors<T> {
-	[key: keyof T]: FieldError[]
+export interface FormErrors {
+	[key: string]: FormError[]
 }
 
-export interface ApiErrors {
-	[key: string]: FieldError[]
-}
-
-export interface LoginFailedResponse {
-	ok: false
+export interface FormError {
 	message: string
-	errors: ApiErrors
+	path: string
+	type: string
 }
 
-export type LoginResponse = LoginSuccesResponse | LoginFailedResponse
-
-export interface VerifySessionResponse {
-	ok: boolean
-	message: string
-	urlRedirect: string
-}
-
-export interface ResponseSuccesResponse {
+// --- Shared Types ---
+export interface SuccessResponse<T = undefined> {
 	ok: true
-	message: string
-	urlRedirect: string
+	message?: string
+	data?: T
+	[key: string]: any
 }
 
-export interface ResponseFailedResponse {
+export interface ErrorResponse {
 	ok: false
 	message: string
-	errors?: ApiErrors
 }
 
-export type RegisterResponse = ResponseSuccesResponse | ResponseFailedResponse
+export interface ValidationErrorResponse {
+	ok: false
+	message: string
+	errors: FormErrors
+}
 
-export type UserHomeResponse =
-	| VerifySessionResponse
-	| {
-			ok: true
-			user: UserHome
-	  }
+export type StandardAPIResponse<T = undefined> =
+	| SuccessResponse<T>
+	| ValidationErrorResponse
 
-export type LogoutResponse =
-	| FetchError
+// --- Auth Routes ---
+/**
+ * Response for the endpoint `/` (GET)
+ */
+export type AuthCheckSessionResponse = { ok: true; urlRedirect: string }
+
+/**
+ * Response for the endpoint `/register` (POST)
+ */
+export type AuthRegisterResponse =
+	| { ok: true; message: string; urlRedirect: string }
+	| ValidationErrorResponse
+
+/**
+ * Response for the endpoint `/login` (POST)
+ */
+export type AuthLoginResponse =
+	| ValidationErrorResponse
 	| {
 			ok: true
 			message: string
 			urlRedirect: string
 	  }
 
-export type GetCandidatesResponse =
-	| FetchError
-	| {
-			ok: true
-			candidates: Candidate[]
-	  }
-
-export type ValidatePermissionsResponse =
-	| FetchError
-	| VerifySessionResponse
-	| {
-			ok: boolean
-			message: string
-	  }
-
-export type GetVoteResponse =
-	| FetchError
-	| {
-			ok: true
-			lastVote: Vote
-	  }
-
-export type VoteResponse =
-	| FetchError
-	| {
-			ok: true
-			message: string
-	  }
-
-export type UpdateProfileResponse =
-	| FetchError
-	| {
-			ok: true
-			message: string
-	  }
-	| {
-			ok: false
-			errors: ApiErrors
-	  }
-
-export interface FindUserUser {
-	email: string
-	id: string
-	timeNewCode: string
-}
-
-export type FindUserResponse =
-	| FetchError
-	| {
-			ok: true
-			user: FindUserUser
-	  }
-	| {
-			ok: false
-			errors: ApiErrors
-	  }
-
-export type SendPasswordResetCodeResponse =
-	| {
-			ok: true
-			timeNewCode: string
-	  }
-	| {
-			ok: false
-			message: string
-			timeNewCode?: string
-	  }
-
-export type VerifyPasswordResetCodeResponse =
-	| {
-			ok: true
-	  }
-	| {
-			ok: false
-			message: string
-			errors?: ApiErrors
-	  }
-
-export type UpdatePasswordResponse =
-	| {
-			ok: true
-			message: string
-			urlReturn: string
-	  }
-	| {
-			ok: false
-			message?: string
-			errors: ApiErrors
-	  }
-
-export type GetCandidateResponse = {
+/**
+ * Response for the endpoint `/logout` (POST)
+ */
+export type AuthLogoutResponse = {
 	ok: true
-	candidate: Candidate
+	message: string
+	urlRedirect: string
 }
 
-export type UpdateCandidateProfileResponse =
+/**
+ * Response for the endpoint `/validate-permissions` (GET)
+ */
+export type AuthValidatePermissionsResponse = { ok: true; message: string }
+
+// --- Config Routes ---
+/**
+ * Response for the endpoint `/config/all` (GET)
+ */
+export type ConfigGetAllResponse = { ok: true; data: Config[] }
+
+/**
+ * Response for the endpoint `/config/:q` (GET)
+ */
+export type ConfigGetOneResponse = { ok: true; data: Config }
+
+/**
+ * Response for the endpoint `/config/` (PUT)
+ */
+export type ConfigUpdateResponse = { ok: true; message: string; config: Config }
+
+/**
+ * Response for the endpoint `/config/:id` (DELETE)
+ */
+export type ConfigDeleteResponse = { ok: true; message: string }
+
+// --- TypeDocument Routes ---
+/**
+ * Response for the endpoint `/typeDocument/all` (GET)
+ */
+export type TypeDocumentGetAllResponse = { ok: true; data: TypeDocument[] }
+
+/**
+ * Response for the endpoint `/typeDocument/:q` (GET)
+ */
+export type TypeDocumentGetOneResponse = { ok: true; data: TypeDocument }
+
+/**
+ * Response for the endpoint `/typeDocument/` (PUT)
+ */
+export type TypeDocumentUpdateResponse = {
+	ok: true
+	message: string
+	typeDocument: TypeDocument
+}
+
+/**
+ * Response for the endpoint `/typeDocument/:id` (DELETE)
+ */
+export type TypeDocumentDeleteResponse = {
+	ok: true
+	message: string
+}
+
+// --- User Routes ---
+/**
+ * Response for the endpoint `/user/` (GET)
+ */
+export type UserGetProfileResponse = {
+	ok: true
+	data: {
+		id: string
+		email: string
+		document: string
+		typeDocumentId: string
+		typeDocument: Pick<TypeDocument, 'id' | 'name'>
+		roleId: string
+		role: Pick<Role, 'id' | 'name'>
+		profile: Pick<Profile, 'name' | 'lastname' | 'phone' | 'imageUrl'>
+	}
+}
+
+/**
+ * Response for the endpoint `/user/notification-token`
+ */
+export type UserUpdateNotificationTokenResponse = { ok: true; message: string }
+
+/**
+ * Response for the endpoint `/user/profile`
+ */
+export type UserUpdateProfileResponse =
+	| { ok: true; message: string }
+	| ValidationErrorResponse
+
+// --- Election Routes ---
+/**
+ * Response for the endpoint `/election/`
+ */
+export type ElectionGetCurrentResponse = { ok: true; data: Election }
+
+/**
+ * Response for the endpoint `/election/` (POST)
+ */
+export type ElectionCreateResponse =
 	| {
 			ok: true
 			message: string
+			election: Election
 	  }
+	| ValidationErrorResponse
+
+/**
+ * Response for the endpoint `/election/finish`
+ */
+export type ElectionFinishResponse =
 	| {
-			ok: false
-			errors: ApiErrors
+			ok: true
+			message: string
+			election: Election
 	  }
+	| ValidationErrorResponse
+
+// --- Candidate Routes ---
+/**
+ * Response for the endpoint `/candidate/` (GET)
+ */
+export type CandidateGetOneResponse = { ok: true; data: Candidate }
+
+/**
+ * Response for the endpoint `/candidate/all`
+ */
+export type CandidateGetAllResponse = { ok: true; data: Candidate[] }
+
+/**
+ * Response for the endpoint `/candidate/` (PATCH)
+ */
+export type CandidateCreateResponse =
+	| { ok: true; message: string }
+	| ValidationErrorResponse
+
+/**
+ * Response for the endpoint `/candidate/:id`
+ */
+export type CandidateDeleteResponse =
+	| { ok: true; message: string }
+	| ValidationErrorResponse
+
+/**
+ * Response for the endpoint `/candidate/vote/:id`
+ */
+export type CandidateVoteResponse =
+	| { ok: true; message: string }
+	| ValidationErrorResponse
+
+/**
+ * Response for the endpoint `/candidate/profile`
+ */
+export type CandidateUpdateProfileResponse =
+	| { ok: true; message: string }
+	| ValidationErrorResponse
+
+// --- Role Routes ---
+/**
+ * Response for the endpoint `/role/all`
+ */
+export type RoleGetAllResponse = { ok: true; data: Role[] }
+
+/**
+ * Response for the endpoint `/role/:q`
+ */
+export type RoleGetOneResponse =
+	| { ok: true; data: Role }
+	| ValidationErrorResponse
+
+/**
+ * Response for the endpoint `/role/` (PUT)
+ */
+export type RoleUpdateResponse =
+	| { ok: true; message: string; role: Role }
+	| ValidationErrorResponse
+
+/**
+ * Response for the endpoint `/role/:id`
+ */
+export type RoleDeleteResponse =
+	| { ok: true; message: string; role: Role }
+	| ValidationErrorResponse
+
+// --- Reset Password Routes ---
+/**
+ * Response for the endpoint `/reset-password/find-user`
+ */
+export type PasswordResetFindUserResponse =
+	| {
+			ok: true
+			user: { id: string; email: string }
+			nextSendAt: Date | false
+	  }
+	| ValidationErrorResponse
+
+/**
+ * Response for the endpoint `/reset-password/send-password-reset-code`
+ */
+export type PasswordResetSendCodeResponse =
+	| { ok: true; message: string; nextSendAt: Date }
+	| { ok: false; message: string; nextSendAt?: Date }
+	| ValidationErrorResponse
+
+/**
+ * Response for the endpoint `/reset-password/verify-password-reset-code`
+ */
+export type PasswordResetVerifyCodeResponse =
+	| { ok: true; message: string }
+	| ValidationErrorResponse
+
+/**
+ * Response for the endpoint `/reset-password/update-password`
+ */
+export type PasswordResetUpdateResponse =
+	| { ok: true; message: string; urlReturn: string }
+	| { ok: false; message: string }
+	| ValidationErrorResponse
