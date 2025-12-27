951d51f16871a386664e22011627368dbb4ffeba
Cristian-F-M <cfmorales.diaz@gmail.com>
2025-12-26 19:48:55 -0500

    Update height to resetPasword layout

--- src/app/(auth)/reset-password/layout.tsx
+++ src/app/(auth)/reset-password/layout.tsx
@@ -58,7 +58,7 @@ export default function ResetPasswordLayout({
 			<div className="absolute top-2 right-2">
 				<ToggleTheme />
 			</div>
-			<div className="border border-gray-300 w-11/12 max-w-[500px] h-auto md:h-11/12 rounded shadow-lg dark:bg-gray-800 dark:border-gray-700">
+			<div className="border border-gray-300 w-11/12 max-w-[500px] rounded shadow-lg dark:bg-gray-800 dark:border-gray-700 min-h-[550px] h-fit">
 				<a
 					className="flex items-center [&_svg]:size-4.5 text-gray-600 text-xs m-1.5 w-fit py-1.5 px-2 rounded hover:bg-gray-200/60 transition-colors dark:text-gray-400 dark:hover:bg-gray-700"
 					href="/login"
