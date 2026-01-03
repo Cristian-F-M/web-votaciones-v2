5dc17b730bedec2f9e2573472e30feeab99fda6b
Cristian-F-M <cfmorales.diaz@gmail.com>
2026-01-02 21:28:23 -0500

    Extract to component ActivateNotificationsModal

--- src/app/apprentice/(election)/no-election/no-election.tsx
+++ src/app/apprentice/(election)/no-election/no-election.tsx
@@ -1,58 +1,20 @@
 'use client'
 import { Button } from '@/components/Button'
-import { Modal } from '@/components/Modal'
 import {
 	IconCalendar,
 	IconBell,
 	IconDeviceMobile,
 	IconCheckbox,
-	IconLogs,
-	IconMail
+	IconLogs
 } from '@tabler/icons-react'
-import { doFetch } from '@/utils/fetch'
-import { useCallback, useEffect, useRef, useState } from 'react'
-import { useUser } from '@/states/useUser'
-import type { UserUpdateNotificationTokenResponse } from '@/types/api'
-import { snackbar } from '@/utils/dom'
+import { useRef } from 'react'
 import '@/styles/no-election.css'
+import { ActivateNotificationsModal } from '@/components/apprentice/ActivateNotificationsModal'
 
 const API_URL = process.env.NEXT_PUBLIC_API_URL
 
 export function NoElection() {
 	const buttonTriggerRef = useRef<HTMLButtonElement>(null)
-	const [areNotificationsActive, setAreNotificationsActive] = useState(false)
-	const { user } = useUser()
-
-	const handleSaveNotificationToken = useCallback(async () => {
-		if (areNotificationsActive)
-			return snackbar({
-				message: 'Ya has activado las notificaciones',
-				variant: 'warning'
-			})
-
-		const response = await doFetch<UserUpdateNotificationTokenResponse>({
-			url: '/user/notification-token',
-			method: 'PATCH',
-			body: {
-				deviceType: 'WEB',
-				notificationToken: user?.email
-			}
-		})
-
-		const variant = response.ok ? 'success' : 'error'
-
-		snackbar({
-			message: response.message,
-			variant
-		})
-
-		setAreNotificationsActive(response.ok)
-	}, [user, areNotificationsActive])
-
-	useEffect(() => {
-		if (!user) return
-		setAreNotificationsActive(!!user.deviceToken)
-	}, [user])
 
 	return (
 		<>
@@ -77,16 +39,9 @@ export function NoElection() {
 					</div>
 
 					<div className="flex md:flex-row flex-col items-center justify-center gap-2 mt-6 w-11/12 md:w-8/12 mx-auto [&_button]:text-sm [&_svg]:size-5">
-						<Button
-							showLoader
-							type="button"
-							ref={buttonTriggerRef}
-							disabled={areNotificationsActive}
-						>
+						<Button showLoader type="button" ref={buttonTriggerRef}>
 							<IconBell />
-							{areNotificationsActive
-								? 'Notificaciones activadas'
-								: 'Activar notificaciones'}
+							Activar notificaciones
 						</Button>
 						<Button
 							type="button"
@@ -152,53 +107,7 @@ export function NoElection() {
 				</a>
 			</span>
 
-			<Modal triggerRef={buttonTriggerRef}>
-				{(closeModal, openModal) => (
-					<>
-						<header className="flex flex-col items-center justify-center gap-0.5">
-							<span className="block text-primary dark:text-dark-primary [&_svg]:size-8 bg-primary/20 dark:bg-dark-primary/20 p-2 rounded-full">
-								<IconBell />
-							</span>
-							<h2 className="text-xl font-semibold">Activar notificaciones</h2>
-						</header>
-
-						<main className="mt-2">
-							<p className="text-sm text-gray-800 dark:text-gray-400 text-center text-pretty">
-								Estás a punto de activar las notificaciones. Recibirás
-								actualizaciones y alertas importantes en tu correo electrónico.
-							</p>
-
-							<div className="mt-2.5 text-center flex flex-row gap-2 items-center [&_svg]:size-5 [&_svg]:text-primary dark:[&_svg]:text-dark-primary [&_svg]:mt-0.5 w-fit mx-auto bg-gray-200/70 dark:bg-gray-700/70 border border-gray-300/70 dark:border-gray-600/70 px-3 py-2 rounded">
-								<IconMail />{' '}
-								<span className="text-sm text-gray-900 dark:text-gray-300">
-									cfmorales.diaz@gmail.com
-								</span>
-							</div>
-
-							<div className="mt-4 flex flex-row gap-2">
-								<Button
-									showLoader={false}
-									type="button"
-									primary={false}
-									onClick={closeModal}
-								>
-									Cancelar
-								</Button>
-								<Button
-									showLoader
-									type="button"
-									onClick={() => {
-										closeModal()
-										setTimeout(handleSaveNotificationToken, 200)
-									}}
-								>
-									Activar
-								</Button>
-							</div>
-						</main>
-					</>
-				)}
-			</Modal>
+			<ActivateNotificationsModal buttonTriggerRef={buttonTriggerRef} />
 		</>
 	)
 }
