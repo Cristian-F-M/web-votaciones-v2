ac6eb70b7536fe3700d288e3513c71cb69b91806
Cristian-F-M <cfmorales.diaz@gmail.com>
2026-01-02 21:28:32 -0500

    Remake component

--- src/components/apprentice/ActivateNotificationsModal.tsx
+++ src/components/apprentice/ActivateNotificationsModal.tsx
@@ -1,121 +1,105 @@
+'use client'
 import { useUser } from '@/states/useUser'
-import { useCallback, useEffect } from 'react'
-import { IconBell } from '@tabler/icons-react'
+import { useCallback, useEffect, useState } from 'react'
+import { IconBell, IconMail } from '@tabler/icons-react'
+import { Modal } from '../Modal'
+import { Button } from '../Button'
+import { snackbar } from '@/utils/dom'
+import { doFetch } from '@/utils/fetch'
+import type { UserUpdateNotificationTokenResponse } from '@/types/api'
 
 export function ActivateNotificationsModal({
 	buttonTriggerRef
 }: { buttonTriggerRef: React.RefObject<HTMLButtonElement | null> }) {
-	const user = useUser((state) => state.user)
-	const maskedEmail = user?.email
-		? (() => {
-				const [username, domain] = user.email.split('@')
-				return `${username.slice(0, 2)}****@${domain}`
-			})()
-		: ''
+	const [areNotificationsActive, setAreNotificationsActive] = useState(false)
+	const [activatingNotifications, setActivatingNotifications] = useState(false)
+	const { user } = useUser()
 
-	const handleClick = useCallback((event: MouseEvent) => {
-		event.preventDefault()
-		handleModal('open')
-	}, [])
+	const handleSaveNotificationToken = useCallback(async () => {
+		if (areNotificationsActive)
+			return snackbar({
+				message: 'Ya has activado las notificaciones',
+				variant: 'warning'
+			})
+
+		setActivatingNotifications(true)
+		const response = await doFetch<UserUpdateNotificationTokenResponse>({
+			url: '/user/notification-token',
+			method: 'PATCH',
+			body: {
+				deviceType: 'WEB',
+				notificationToken: user?.email
+			}
+		})
+		setActivatingNotifications(false)
+
+		const variant = response.ok ? 'success' : 'error'
+
+		snackbar({
+			message: response.message,
+			variant
+		})
+
+		setAreNotificationsActive(response.ok)
+	}, [user, areNotificationsActive])
 
 	useEffect(() => {
-		buttonTriggerRef.current?.addEventListener('click', handleClick)
-		return () => {
-			buttonTriggerRef.current?.removeEventListener('click', handleClick)
-		}
-	}, [buttonTriggerRef, handleClick])
-
-	const handleModal = useCallback((type: 'close' | 'open') => {
-		const action = type === 'open' ? 'showModal' : 'close'
-		const modal = document.querySelector(
-			'#activate-notifications-modal'
-		) as HTMLDialogElement
-
-		modal?.[action]()
-	}, [])
-
-  // useEffect(() => {
-  //   // handleModal('open')
-  // }, [handleModal])
-
+		if (!user) return
+		setAreNotificationsActive(!!user.deviceToken)
+	}, [user])
 
 	return (
-		<>
-			{/* <style>{`
-      dialog::backdrop {
-        background-color: rgba(0, 0, 0, 0.5);
-        z-index: 10;
-        display: flex;
-        flex-direction: column;
-        justify-content: center;
-        align-items: center;
-      }
+		<Modal triggerRef={buttonTriggerRef}>
+			{(closeModal, openModal) => (
+				<>
+					<header className="flex flex-col items-center justify-center gap-0.5">
+						<span className="block text-primary dark:text-dark-primary [&_svg]:size-8 bg-primary/20 dark:bg-dark-primary/20 p-2 rounded-full">
+							<IconBell />
+						</span>
+						<h2 className="text-xl font-semibold">Activar notificaciones</h2>
+					</header>
 
-        dialog[open] {
-          display: flex;
-          scale: 1;
-          opacity: 1;
-
-
-          @starting-style {
-            scale: 0.8;
-            opacity: 0;
-          }
-        }
-
-
-        dialog {
-          display: none;
-          scale: 0.8;
-          opacity: 0;
-          transition: all 0.3s allow-discrete;
-        }
-
-    `}</style> */}
-
-			<dialog
-				className="m-auto rounded shadow flex-col p-5 w-11/12 md:max-w-[450px]"
-				id="activate-notifications-modal"
-			>
-				<header className="flex flex-row gap-1 items-center ">
-					<IconBell className="text-primary size-5" />
-					<h3 className="md:text-base text-sm font-semibold">
-						Activar notificaciones por email
-					</h3>
-				</header>
-				<main className="mt-px">
-					<p className="text-gray-700 md:text-sm text-xs dark:text-gray-400">
-						Se enviará un correo electrónico a{' '}
-						<span className="text-primary dark:text-dark-primary font-semibold">
-							{maskedEmail}
-						</span>{' '}
-						cuando haya nuevas votaciones
-					</p>
-
-					<div className="bg-primary/20 px-3 py-2 rounded mt-3 border border-primary/30 text-xs">
-						<p>
-							📧 Recibiras una notificación inmediata cuando se abran nuevos
-							procesos de votacion.
+					<main className="mt-2">
+						<p className="text-sm text-gray-800 dark:text-gray-400 text-center text-pretty">
+							Estás a punto de activar las notificaciones. Recibirás
+							actualizaciones y alertas importantes en tu correo electrónico.
 						</p>
-					</div>
 
-					<div className="mt-3 flex md:flex-row flex-col-reverse gap-2 justify-end [&_button]:px-3 [&_button]:py-1.5 [&_button]:rounded [&_button]:text-sm [&_button]:cursor-pointer">
-						<button
-							className="border border-gray-300 hover:bg-gray-300/30 dark:border-gray-600 dark:hover:bg-gray-600/30 active:bg-gray-300/30 dark:active:bg-gray-600/30"
-							type="button"
-							onClick={() => handleModal('close')}
-						>
-							Cancelar
-						</button>
-						<button
-							className="bg-primary hover:bg-primary/80 dark:bg-dark-primary/80 dark:hover:bg-dark-primary/60 active:bg-primary/60 dark:active:bg-dark-primary/60"
-							type="button"
-						>
-							Activar notificaciones
-						</button>
-					</div>
-				</main>
-			</dialog>
-		</>
+						<div className="mt-2.5 text-center flex flex-row gap-2 items-center [&_svg]:size-5 [&_svg]:text-primary dark:[&_svg]:text-dark-primary [&_svg]:mt-0.5 w-fit mx-auto bg-gray-200/70 dark:bg-gray-700/70 border border-gray-300/70 dark:border-gray-600/70 px-3 py-2 rounded">
+							<IconMail />{' '}
+							<span className="text-sm text-gray-900 dark:text-gray-300">
+								cfmorales.diaz@gmail.com
+							</span>
+						</div>
+
+						<div className="mt-4 flex flex-row gap-2">
+							{!areNotificationsActive && (
+								<Button
+									showLoader={false}
+									type="button"
+									primary={false}
+									onClick={closeModal}
+								>
+									Cancelar
+								</Button>
+							)}
+							<Button
+								showLoader
+								type="button"
+								disabled={activatingNotifications || areNotificationsActive}
+								onClick={() => {
+									closeModal()
+									setTimeout(handleSaveNotificationToken, 200)
+								}}
+							>
+								{!areNotificationsActive
+									? 'Activar'
+									: 'Ya has hactivado las notificaciones'}
+							</Button>
+						</div>
+					</main>
+				</>
+			)}
+		</Modal>
 	)
 }
