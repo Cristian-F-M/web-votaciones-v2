c580e7a20581764a1fd919d8af94cf3c7b94b983
Cristian-F-M <cfmorales.diaz@gmail.com>
2026-01-02 21:06:58 -0500

    Remake CoundDown component

--- src/components/SingleTimeCard.tsx
+++ src/components/SingleTimeCard.tsx
@@ -3,11 +3,9 @@ export function SingleTimeCard({
 	value
 }: { label: string; value: string }) {
 	return (
-		<div className="single-time flex flex-col justify-center items-center">
-			<span className="time-value text-primary dark:text-dark-primary md:text-2xl text-base">
-				{value}
-			</span>
-			<span className="time-label dark:text-gray-400 md:text-sm text-xs">
+		<div className="single-time flex flex-col justify-center items-center bg-primary/80 dark:bg-dark-primary/80 text-page-contrast py-5 px-10 rounded-lg w-32 md:w-40">
+			<span className="time-value md:text-4xl text-3xl">{value}</span>
+			<span className="time-label text-gray-800 dark:text-gray-300 md:text-base text-sm">
 				{label}
 			</span>
 		</div>
--- src/components/apprentice/CountDown.tsx
+++ src/components/apprentice/CountDown.tsx
@@ -1,26 +1,22 @@
 import { SingleTimeCard } from '../SingleTimeCard'
 import { useRemainingTime } from '@/hooks/useRemainingTime'
 import { IconClock } from '@tabler/icons-react'
+import { twMerge } from 'tailwind-merge'
 
-export function CountDown({
-	targetDate
-}: { targetDate: Date | string | null }) {
+interface CountDownProps extends React.HTMLAttributes<HTMLDivElement> {
+	targetDate: Date | string | null
+}
+
+export function CountDown({ targetDate, className, ...props }: CountDownProps) {
 	const { remainingTime } = useRemainingTime(targetDate)
 
 	return (
-		<div className="remaining-time w-10/12 mx-auto mt-7">
-			<h3 className="flex flex-row gap-1 items-center w-fit mx-auto md:text-xl text-sm">
-				<IconClock className="text-primary dark:text-dark-primary size-6" />{' '}
-				Tiempo restante para votar
-			</h3>
-			<div className="mx-auto w-fit mt-2 flex flex-row gap-1 items-center justify-center border dark:border-primary/50 border-primary md:px-14 px-5 py-4 rounded">
+		<div className={twMerge('w-full md:w-10/12 mx-auto', className)} {...props}>
+			<div className="mx-auto w-fit mt-2 flex flex-wrap md:flex-row gap-3 items-center justify-center md:px-6 px-0 py-6 rounded">
 				<SingleTimeCard label="días" value={remainingTime.days} />
-				<span className="time-separator">:</span>
 				<SingleTimeCard label="horas" value={remainingTime.hours} />
-				<span className="time-separator">:</span>
-				<SingleTimeCard label="min" value={remainingTime.minutes} />
-				<span className="time-separator">:</span>
-				<SingleTimeCard label="seg" value={remainingTime.seconds} />
+				<SingleTimeCard label="minutos" value={remainingTime.minutes} />
+				<SingleTimeCard label="segundos" value={remainingTime.seconds} />
 			</div>
 		</div>
 	)
