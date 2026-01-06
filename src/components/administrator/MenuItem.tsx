38bcdad8bd608fdd2424acb7b9afbb4af6c946c9
Cristian-F-M <cfmorales.diaz@gmail.com>
2026-01-06 16:23:43 -0500

    Add MenuItem component

---
+++ src/components/administrator/MenuItem.tsx
@@ -0,0 +1,36 @@
+'use client'
+import type { MenuItemProps } from '@/types/SideMenu'
+import * as Icons from '@tabler/icons-react'
+import { usePathname } from 'next/navigation'
+import { twMerge } from 'tailwind-merge'
+
+export function MenuItem({ href, prefix, className, ...props }: MenuItemProps) {
+	const url = `${prefix ?? ''}${href}`
+	const active = usePathname() === url
+	const isDeclarative = 'label' in props && 'icon' in props
+	const Icon = isDeclarative
+		? (Icons[props.icon ?? 'IconExternalLink'] as React.ElementType)
+		: undefined
+
+	return (
+		<a
+			onClick={(event) => {
+				if (active) event.preventDefault()
+			}}
+			className={twMerge(
+				'px-2 py-2.5 rounded hover:bg-primary/50 hover:dark:bg-dark--primary/50 hover:border-transparent text-sm flex flex-row items-center gap-2 [&_svg]:size-5 [&.active]:bg-primary [&.active]:dark:bg-dark-primary [&.active]:border-transparent transition-colors bg-gray-200 border border-gray-300 dark:bg-gray-700 dark:border-gray-600',
+				active && 'active',
+				className
+			)}
+			href={url}
+		>
+			{!isDeclarative && props.children}
+			{isDeclarative && (
+				<>
+					{Icon && <Icon />}
+					{props.label}
+				</>
+			)}
+		</a>
+	)
+}
