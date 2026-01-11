3820ca4a169b7f254183a85a0df7c79d851c1862
Cristian-F-M <cfmorales.diaz@gmail.com>
2026-01-11 12:54:38 -0500

    Add function to do deep search with each complex item

--- src/utils/table.ts
+++ src/utils/table.ts
@@ -1,3 +1,7 @@
+import type { CustomObject } from '@/types'
+import type { TableSingleItem } from '@/types/table'
+import { includes } from '@/utils/global'
+
 export function getModelsNames(...keys: (string | undefined)[]) {
 	return keys.map((k) => {
 		if (!k) return
@@ -10,4 +14,25 @@ export function getModelsNames(...keys: (string | undefined)[]) {
 
 		return newKey
 	})
+}
+
+export function deepSearch(
+	item: CustomObject<TableSingleItem> | TableSingleItem,
+	query: string
+): boolean {
+	const lowerQuery = query.toLowerCase().trim()
+	const lowerItem = typeof item === 'string' ? item.toLowerCase() : item
+
+	if (!item) return false
+	if (typeof lowerItem === 'string') return includes(lowerItem, lowerQuery)
+
+	const isArray = Array.isArray(item)
+	const isObject = !isArray && typeof item === 'object'
+
+	if (isArray) return item.some((i) => deepSearch(i, lowerQuery))
+
+	if (isObject)
+		return Object.values(item).some((i) => deepSearch(i, lowerQuery))
+
+	return false
 }
