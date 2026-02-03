7c3fed97d8d674fb53d2d415be251902cbb085a0
Cristian-F-M <cfmorales.diaz@gmail.com>
2026-02-02 20:08:40 -0500

    Add useDebounce hook

--- src/utils/global.ts
+++ src/utils/global.ts
@@ -1,3 +1,5 @@
+import { useEffect, useState } from 'react'
+
 export function normalize(str: string) {
 	// biome-ignore lint/suspicious/noMisleadingCharacterClass: <explanation>
 	return str.normalize('NFD').replace(/[\u0300-\u036f]/g, '')
@@ -6,4 +8,18 @@ export function normalize(str: string) {
 export function includes(b: string, s: string) {
 	const regex = new RegExp(normalize(s), 'i')
 	return regex.test(normalize(b))
+}
+
+export function useDobounce<T>(value: T, delay: number) {
+	const [debouncedValue, setDebouncedValue] = useState<T>(value)
+
+	useEffect(() => {
+		const timeout = setTimeout(() => {
+			setDebouncedValue(value)
+		}, delay)
+
+		return () => clearTimeout(timeout)
+	}, [value, delay])
+
+	return debouncedValue
 }
