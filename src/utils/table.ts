5a42423160c4f97401429d9622be15fc63086ec5
Cristian-F-M <cfmorales.diaz@gmail.com>
2026-02-03 17:26:54 -0500

    Add functions to sort items

--- src/utils/table.ts
+++ src/utils/table.ts
@@ -1,6 +1,7 @@
 import type { CustomObject } from '@/types'
-import type { TableSingleItem } from '@/types/table'
+import type { TableItems, TableSingleItem } from '@/types/table'
 import { includes } from '@/utils/global'
+import { useCallback, useMemo } from 'react'
 
 export function getModelsNames(...keys: (string | undefined)[]) {
 	return keys.map((k) => {
@@ -41,4 +42,43 @@ export function deepSearch(
 		return Object.values(item).some((i) => deepSearch(i, lowerQuery))
 
 	return false
+}
+
+// biome-ignore lint/suspicious/noExplicitAny: <explanation>
+export function getValueType(value: any) {
+	if (value instanceof Date) return 'date'
+
+	if (!Number.isNaN(Date.parse(value)) && typeof value === 'string') {
+		return 'date'
+	}
+
+	if (typeof value === 'number') return 'number'
+
+	return 'string'
+}
+
+
+export function sortData
+	(data: TableItems, key: string, direction: 'ASC' | 'DESC') {
+	return [...data].sort((a, b) => {
+		// biome-ignore lint/suspicious/noExplicitAny: <explanation>
+		let valA: any = a[key]
+		// biome-ignore lint/suspicious/noExplicitAny: <explanation>
+		let valB: any = b[key]
+
+		const type = getValueType(valA)
+
+		if (type === 'date') {
+			valA = new Date(valA).getTime()
+			valB = new Date(valB).getTime()
+		}
+
+		if (type === 'number') {
+			return direction === 'ASC' ? valA - valB : valB - valA
+		}
+
+		return direction === 'ASC'
+			? String(valA).localeCompare(String(valB))
+			: String(valB).localeCompare(String(valA))
+	})
 }
